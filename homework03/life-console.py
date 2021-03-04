7bf9f581100de5bef3e9d075d49ecca12209530d
IVladimirA <vova@t0lik.com>
2021-03-04 21:41:43 +0300

    Class Console improved

--- homework03/life-console.py
+++ homework03/life-console.py
@@ -6,41 +6,51 @@ from ui import UI
 
 
 class Console(UI):
-    def __init__(self, life: GameOfLife) -> None:
+    def __init__(self, life: GameOfLife, sleeptime: int = 1000) -> None:
         super().__init__(life)
+        self.sleeptime = sleeptime
 
-    def draw_borders(self, screen) -> None:
+    def draw_borders(self, window) -> None:
         for i in range(0, self.life.cols + 1):
-            screen.addch(0, i, "-")
-            screen.addch(self.life.rows + 1, i, "-")
+            window.addch(0, i, "-")
+            window.addch(self.life.rows + 1, i, "-")
         for i in range(0, self.life.rows + 1):
-            screen.addch(i, 0, "|")
-            screen.addch(i, self.life.cols + 1, "|")
-        screen.addch(0, 0, "+")
-        screen.addch(self.life.rows + 1, 0, "+")
-        screen.addch(0, self.life.cols + 1, "+")
-        screen.addch(self.life.rows + 1, self.life.cols + 1, "+")
+            window.addch(i, 0, "|")
+            window.addch(i, self.life.cols + 1, "|")
+        window.addch(0, 0, "+")
+        window.addch(self.life.rows + 1, 0, "+")
+        window.addch(0, self.life.cols + 1, "+")
+        window.addch(self.life.rows + 1, self.life.cols + 1, "+")
 
-    def draw_grid(self, screen) -> None:
+    def draw_grid(self, window) -> None:
         for i in range(0, self.life.rows):
             for j in range(0, self.life.cols):
                 if self.life.curr_generation[i][j] == 1:
-                    screen.addch(i + 1, j + 1, "*")
+                    window.addch(i + 1, j + 1, "*")
                 else:
-                    screen.addch(i + 1, j + 1, " ")
+                    window.addch(i + 1, j + 1, " ")
 
     def run(self) -> None:
-        screen = curses.initscr()
-        screen = curses.newwin(self.life.rows + 2, self.life.cols + 3, 0, 0)
-        screen.move(0, 0)
-        while not self.life.is_max_generations_exceeded:
-            self.draw_borders(screen)
-            self.draw_grid(screen)
-            screen.refresh()
-            self.life.step()
-            time.sleep(0.5)
+        curses.initscr()
+        curses.noecho()
+        curses.cbreak()
+        window = curses.newwin(self.life.rows + 2, self.life.cols + 3, 0, 0)
+        window.nodelay(True)
+        pause = False
+        while True:
+            key = window.getch()
+            if key == ord("q") or key == ord("Q"):
+                break
+            if key == ord("p") or key == ord("P"):
+                pause = not pause
+            if not pause:
+                self.draw_borders(window)
+                self.draw_grid(window)
+                window.refresh()
+                self.life.step()
+                curses.napms(self.sleeptime)
         curses.endwin()
 
 
-# my_game = Console(GameOfLife((8, 8), True, 50))
+# my_game = Console(GameOfLife((8, 8), True, 10), 1000)
 # my_game.run()
