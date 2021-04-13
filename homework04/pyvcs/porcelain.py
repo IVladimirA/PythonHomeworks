b9e35d489370fccf7d34527ba15e550ce51abd6d
IVladimirA <vova@t0lik.com>
2021-04-13 07:09:43 +0300

    Функции add() и commit() реализованы.

--- homework04/pyvcs/porcelain.py
+++ homework04/pyvcs/porcelain.py
@@ -9,15 +9,51 @@ from pyvcs.tree import commit_tree, write_tree
 
 
 def add(gitdir: pathlib.Path, paths: tp.List[pathlib.Path]) -> None:
-    # PUT YOUR CODE HERE
-    ...
+    update_index(gitdir, paths)
 
 
 def commit(gitdir: pathlib.Path, message: str, author: tp.Optional[str] = None) -> str:
-    # PUT YOUR CODE HERE
-    ...
+    commit_sha = commit_tree(gitdir, write_tree(gitdir, read_index(gitdir)), message, author=author)
+    if is_detached(gitdir):
+        ref = gitdir / "HEAD"
+    else:
+        ref = pathlib.Path(get_ref(gitdir))
+    # print(f"REF: {ref}")
+    f = open(gitdir / ref, "w")
+    f.write(commit_sha)
+    f.close()
+    return commit_sha
 
 
 def checkout(gitdir: pathlib.Path, obj_name: str) -> None:
-    # PUT YOUR CODE HERE
-    ...
+    fmt, old_content = read_object(obj_name, gitdir)
+    old_content_s = old_content.decode()
+    objects = find_tree_files(old_content_s[5:25], gitdir)
+    """print(f"CONTENTS of {obj_name}:")
+    for obj in objects:
+        print(obj[0], obj[1])
+    print("")"""
+    project_dir = gitdir.absolute().parent
+    print("PROJECT DIR", project_dir)
+    old_dirs = []
+    for obj in objects:
+        if os.path.isfile(project_dir / obj[0]):
+            os.remove(project_dir / obj[0])
+        elif os.path.isdir(project_dir / obj[0]):
+            if len(os.listdir(project_dir / obj[0])) == 0:
+                os.rmdir(project_dir / obj[0])
+            else:
+                old_dirs.append(project_dir / obj[0])
+    cur_dir_i, cnt = 0, 0
+    removed = [False] * len(old_dirs)
+    while cnt < len(old_dirs):
+        if not removed[cur_dir_i] and len(os.listdir(old_dirs[cur_dir_i])) == 0:
+            os.rmdir(old_dirs[cur_dir_i])
+            removed[cur_dir_i] = True
+            cnt += 1
+        cur_dir_i += 1
+        if cur_dir_i >= len(old_dirs):
+            cur_dir_i = 0
+    print("CONTENT OF GITDIR: ")
+    print(os.listdir(project_dir))
+    print("")
--- homework04/pyvcs/refs.py
+++ homework04/pyvcs/refs.py
@@ -3,30 +3,52 @@ import typing as tp
 
 
 def update_ref(gitdir: pathlib.Path, ref: tp.Union[str, pathlib.Path], new_value: str) -> None:
-    # PUT YOUR CODE HERE
-    ...
+    ref_path = gitdir / ref
+    pathlib.Path(ref_path).touch()
+    f = open(ref_path, "w")
+    f.write(new_value)
+    f.close()
 
 
 def symbolic_ref(gitdir: pathlib.Path, name: str, ref: str) -> None:
-    # PUT YOUR CODE HERE
-    ...
+    f = open(gitdir / name, "w")
+    f.write(ref)
+    f.close()
 
 
 def ref_resolve(gitdir: pathlib.Path, refname: str) -> str:
-    # PUT YOUR CODE HERE
-    ...
+    if refname == "HEAD":
+        f = open(gitdir / "HEAD", "r")
+        refname = f.read()[5:-1]
+        f.close()
+    f = open(gitdir / refname, "r")
+    data = f.read()
+    f.close()
+    return data
 
 
 def resolve_head(gitdir: pathlib.Path) -> tp.Optional[str]:
-    # PUT YOUR CODE HERE
-    ...
+    if pathlib.Path.exists(gitdir / get_ref(gitdir)):
+        return ref_resolve(gitdir, "HEAD")
+    return None
 
 
 def is_detached(gitdir: pathlib.Path) -> bool:
-    # PUT YOUR CODE HERE
-    ...
+    if not pathlib.Path.exists(gitdir / "HEAD"):
+        return False
+    f = open(gitdir / "HEAD", "r")
+    data = f.read()
+    f.close()
+    if type(data) == str and len(data) == 40 and data[:5] != "ref: ":
+        return True
+    return False
 
 
 def get_ref(gitdir: pathlib.Path) -> str:
-    # PUT YOUR CODE HERE
-    ...
+    f = open(gitdir / "HEAD", "r")
+    ref = f.read()
+    f.close()
+    ref = ref[:-1]
+    if ref[:5] == "ref: ":
+        ref = ref[5:]
+    return ref
