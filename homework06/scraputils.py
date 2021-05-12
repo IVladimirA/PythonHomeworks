5350598c5cb6cd9b26b4ed9ff9f59e1bcfb70200
IVladimirA <vova@t0lik.com>
2021-05-12 14:14:41 +0300

    Реализвоны функции для получения словаря новостей

--- homework06/scraputils.py
+++ homework06/scraputils.py
@@ -5,15 +5,39 @@ from bs4 import BeautifulSoup
 def extract_news(parser):
     """ Extract news from a given web page """
     news_list = []
-
-    # PUT YOUR CODE HERE
-
+    table = parser.body.center.table
+    rows = []
+    for row in table.findAll("tr"):
+        rows.append(row)
+    content = rows[3].findAll("tr")
+    for i in range(0, len(content) - 3, 3):
+        page = dict()
+        links = content[i + 1].findAll("td")[1].findAll("a")
+        page["points"] = int(content[i + 1].findAll("td")[1].span.text.split()[0])
+        page["author"] = links[0].text
+        comments = "discuss"
+        if len(links) == 4:
+            comments = links[3].text.split()[0]
+        if comments == "discuss":
+            page["comments"] = 0
+        else:
+            page["comments"] = int(comments)
+        link = content[i].findAll("td")[2].find("a")
+        page["url"] = link["href"]
+        page["title"] = link.text
+        news_list.append(page)
     return news_list
 
 
 def extract_next_page(parser):
     """ Extract next page URL """
-    # PUT YOUR CODE HERE
+    table = parser.body.center.table
+    rows = []
+    for row in table.findAll("tr"):
+        rows.append(row)
+    content = rows[3].findAll("tr")
+    page = content[-1].findAll("td")[1]
+    return page.find("a")["href"]
 
 
 def get_news(url, n_pages=1):
@@ -30,3 +54,6 @@ def get_news(url, n_pages=1):
         n_pages -= 1
     return news
 
+
+# test = get_news("https://news.ycombinator.com", 2)
+# print(test[35], "\n", test[5])
